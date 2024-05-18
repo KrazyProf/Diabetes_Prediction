9729f59d5e30496dd7c1b58800939cbcae4d5b3c
Ayush Kaithwas <ayushkaithwas860@gmail.com>
2024-05-19 00:40:19 +0530

    model trainer completed

--- src/diabetes_classification/components/data_transformation.py
+++ src/diabetes_classification/components/data_transformation.py
@@ -12,7 +12,6 @@ from sklearn.preprocessing import StandardScaler
 
 from diabetes_classification.exception.exception import CustomException
 from diabetes_classification.logging.logger import logging
-from diabetes_classification.utils.utlis import save_object
 
 @dataclass
 class DataTransformationConfig:
@@ -48,6 +47,23 @@ class DataTransformation:
         
         except Exception as e:
             raise CustomException(e, sys)
+
+    def remove_outliers(self, df, columns):
+        try:
+            # Removing outliers from numerical columns using IQR method
+            for col in columns:
+                Q1 = df[col].quantile(0.25)
+                Q3 = df[col].quantile(0.75)
+                IQR = Q3 - Q1
+                lower_bound = Q1 - 1.5 * IQR
+                upper_bound = Q3 + 1.5 * IQR
+                df = df[(df[col] >= lower_bound) & (df[col] <= upper_bound)]
+
+
+            return df
+
+        except Exception as e:
+            raise CustomException(e, sys)
         
     def initiate_data_transformation(self, train_path, test_path):
         try:
@@ -59,11 +75,18 @@ class DataTransformation:
 
             preprocessing_obj = self.get_data_transformer_object()
 
-            drop_columns = ['MentHlth' , 'PhysHlth']
+            drop_columns = ['Education', 'Income']
             target_column_name = "Diabetes_binary"
 
+            outlier_columns = ["BMI" , "MentHlth" , "PhysHlth"]
+           
+            
             train_df = train_df.drop(columns=drop_columns, axis=1)
             test_df = test_df.drop(columns=drop_columns, axis=1)
+
+            # Removing outliers
+            train_df = self.remove_outliers(train_df,outlier_columns)
+            test_df = self.remove_outliers(test_df,outlier_columns)
 
             input_feature_train_df = train_df.drop(columns=[target_column_name], axis=1)
             target_feature_train_df = train_df[target_column_name]
@@ -94,5 +117,10 @@ class DataTransformation:
         except Exception as e:
             raise CustomException(e, sys)
 
-
+def save_object(file_path, obj):
+    try:
+        with open(file_path, 'wb') as file:
+            pickle.dump(obj, file)
+    except Exception as e:
+        raise CustomException(e, sys)
 
